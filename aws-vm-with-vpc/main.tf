187ab21866dcdc7affa89c5ef08d23ebfdea110c
Gustavo Ziaugra <gziaugra@Gustavos-MacBook-Air.local>
2025-10-27 16:13:31 -0400

    Add VM with VPC into AWS

---
+++ aws-vm-with-vpc/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = "~> 1.13.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "remote-state-bucket"
+    key    = "aws-vm-with-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = "Development"
+      Owner       = "Terraform LocalStack Management"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "remote-state-bucket"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
