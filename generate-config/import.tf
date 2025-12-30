87212fafeb1009335dafcb2145d8bb67346865db
Gustavo Ziaugra <gustavo.ziaugra@live.com>
2025-12-30 13:00:00 -0500

    Exercice terraform generate config

---
+++ generate-config/import.tf
@@ -0,0 +1,14 @@
+import {
+  to = aws_vpc.aws_vpc
+  id = "vpc-0abcd1234efgh5678"
+}
+
+import {
+  to = aws_subnet.aws_subnet_public_1
+  id = "subnet-0abcd1234efgh5678"
+}
+
+import {
+  to = aws_subnet.aws_subnet_public_2
+  id = "subnet-1abcd1234efgh5678"
+}
